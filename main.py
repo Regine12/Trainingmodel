0820bf231feac9ffe1c81c7e10d516e1f6ae6a4a
Regine12 <reginecyrille@gmail.com>
2022-05-31 20:36:42 +0100

    good

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ from werkzeug.utils import secure_filename
 import os
 from wtforms.validators import InputRequired
 
+
+#image processing 
+import cv2
+from PIL import Image
+import numpy as np
+
+import tensorflow as tf
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'static/files'
@@ -14,13 +22,19 @@ class UploadFileForm(FlaskForm):
     submit = SubmitField("Upload File")
 
 @app.route('/', methods=['GET',"POST"])
-@app.route('/home', methods=['GET',"POST"])
+# @app.route('/home', methods=['GET',"POST"])
 def home():
     form = UploadFileForm()
     if form.validate_on_submit():
         file = form.file.data # First grab the file
         file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
-        return "File has been uploaded."
+        
+        # get image path
+        image = os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))
+        # Load model and pass the image
+        
+        # And Return Predictions
+        return image
     return render_template('index.html', form=form)
 
 if __name__ == '__main__':
